9d31b08670ea91ea2c83df2a58275a1ce07d84fd
Ezequiel Rueda <ezewheel@gmail.com>
2026-02-24 11:34:28 -0300

    Minox fix

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://hemalink-api-e7aeh2fydcasged8.eastus-01.azurewebsites.net')
+  }
 })
